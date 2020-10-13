4a253551d1aba4795345f7905d49ac96276b3cae
MichaelJahns <jahnsmic001@gmail.com>
2020-10-13 12:00:11 -0700

    Finds the template image in the source! Successfully able to opencv to find images within other images

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,15 @@
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.awt.image.ImageObserver;
-import java.text.AttributedCharacterIterator;
-
 public class Main {
     static {System.loadLibrary(Core.NATIVE_LIBRARY_NAME);}
-
-    public static Image toBufferedImage(Mat m){
-        int type = BufferedImage.TYPE_BYTE_GRAY;
-        if(m.channels() != 1){
-            Mat m2 = new Mat();
-            Imgproc.cvtColor(m, m2, Imgproc.COLOR_BGR2RGB);
-            m=m2;
-        }
-        byte[] b = new byte[m.channels()*m.cols()*m.rows()];
-        m.get(0,0,b);
-        BufferedImage image = new BufferedImage(m.cols(), m.rows(), type);
-        image.getRaster().setDataElements(0,0,m.cols(), m.rows(), b);
-        return image;
-    }
-
     public static void main(String args[]){
+        System.out.println(Core.VERSION);
 
         String beach = "images/haystack.jpg";
         String seal = "images/needle.jpg";
@@ -34,19 +17,18 @@ public class Main {
         Mat haystackImg = Imgcodecs.imread(beach);
         Mat needleImg = Imgcodecs.imread(seal);
 
+        Mat diff = new Mat();
+        int machMethod = Imgproc.TM_CCOEFF_NORMED;
+        Imgproc.matchTemplate(haystackImg, needleImg, diff, machMethod);
 
-        Image bufferedImage = toBufferedImage(haystackImg);
-        Graphics g = bufferedImage.getGraphics();
-        g.drawImage(bufferedImage, 0, 0, null);
-        System.out.println("success");
-        System.out.println(Core.VERSION);
+        Core.MinMaxLocResult mmr = Core.minMaxLoc(diff);
+        Point matchLocation = mmr.maxLoc;
+        Imgproc.rectangle(haystackImg, matchLocation, new Point(matchLocation.x + needleImg.cols(), matchLocation.y+needleImg.rows()), new Scalar(255,255,255));
+        Imgcodecs.imwrite(beach+"sonuc.jpg", haystackImg);
 
-        JFrame frame= new JFrame("Welecome to JavaTutorial.net");
-        frame.getContentPane().add(new JFrameGraphics());
-        frame.setSize(600, 400);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
+
+        HighGui.imshow("Beach", needleImg);
+        HighGui.waitKey();
     }
 
 

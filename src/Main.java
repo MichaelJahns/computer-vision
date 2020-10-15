e21b43363cec14f0c676aa3668234bef80995ed5
MichaelJahns <jahnsmic001@gmail.com>
2020-10-14 17:32:51 -0700

    All day stab at getting read outs on the other five matchTemplating methods, I feel as though the all Black and all White returns are a mark that ive done something wrong

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,77 @@
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
+import org.opencv.core.*;
 import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
 public class Main {
-    static {System.loadLibrary(Core.NATIVE_LIBRARY_NAME);}
-    public static void main(String args[]){
-        System.out.println(Core.VERSION);
-
-        String beach = "images/haystack.jpg";
-        String seal = "images/needle.jpg";
-
-        Mat haystackImg = Imgcodecs.imread(beach);
-        Mat needleImg = Imgcodecs.imread(seal);
-
-        Mat diff = new Mat();
-        int machMethod = Imgproc.TM_CCOEFF_NORMED;
-        Imgproc.matchTemplate(haystackImg, needleImg, diff, machMethod);
-
-        Core.MinMaxLocResult mmr = Core.minMaxLoc(diff);
-        Point matchLocation = mmr.maxLoc;
-        Imgproc.rectangle(haystackImg, matchLocation, new Point(matchLocation.x + needleImg.cols(), matchLocation.y+needleImg.rows()), new Scalar(255,255,255));
-        Imgcodecs.imwrite(beach+"sonuc.jpg", haystackImg);
-
-
-        HighGui.imshow("Beach", needleImg);
-        HighGui.waitKey();
+    static {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     }
 
+    public static int nWidth;
+    public static int nHeight;
+    public static double threshold;
+
+    public static void main(String args[]) {
+        System.out.println(Core.VERSION);
+
+        String haystackPath = "images/albion_farm.jpg";
+        String needlePath = "images/albion_cabbage.jpg";
+
+        Mat haystackImg = Imgcodecs.imread(haystackPath);
+        Mat grayHaystack = new Mat(haystackImg.rows(), haystackImg.cols(), CvType.CV_8U);
+        Imgproc.cvtColor(haystackImg, grayHaystack, Imgproc.COLOR_BGR2GRAY);
+
+        Mat needleImg = Imgcodecs.imread(needlePath);
+        Mat grayNeedle = new Mat(needleImg.rows(), needleImg.cols(), CvType.CV_8U);
+        Imgproc.cvtColor(needleImg, grayNeedle, Imgproc.COLOR_BGR2GRAY);
+        nWidth = needleImg.cols();
+        nHeight = needleImg.rows();
+
+        threshold = 0.8;
+
+        Mat matchTemplate = new Mat(grayHaystack.rows(), grayHaystack.cols(), CvType.CV_8U);
+        int machMethod = Imgproc.TM_CCOEFF_NORMED;
+        Imgproc.matchTemplate(grayHaystack, grayNeedle, matchTemplate, machMethod);
+        Imgcodecs.imwrite("out/differenceMaps/matchTemplateAlbion" + machMethod + ".jpg", matchTemplate);
+
+        HighGui.imshow("fart", matchTemplate);
+        HighGui.waitKey();
+
+
+//        System.out.println(matchTemplate.depth());
+////data type 5 is cv32fc1 or 32bits floating point one channel
+////        How does the above information help me to iterate over it?
+//        Mat thresholdImage = new Mat();
+//        int thresholdMethod = Imgproc.THRESH_TOZERO;
+//        Imgproc.threshold(matchTemplate, thresholdImage ,threshold, 255, thresholdMethod);
+//        Imgcodecs.imwrite("out/differenceMaps/Gthreshold"+ thresholdMethod + ".jpg", thresholdImage);
+
+
+//            Core.MinMaxLocResult mmr = Core.minMaxLoc(diff);
+//            Point matchLocation = mmr.maxLoc;
+//
+//            drawRectangle(haystackImg, matchLocation);
+
+
+//            HighGui.imshow("Beach", thresholdImage);
+//            HighGui.waitKey();
+
+    }
+
+    public static void drawRectangle(Mat source, Point location) {
+        Imgproc.rectangle(
+                source,
+                location,
+                new Point(location.x + nWidth, location.y + nHeight),
+                new Scalar(255, 255, 255)
+        );
+//        iterate diff I think
+//        HOW DO I ITERATE DIFF
+//        filter confidences less that threshold
+//        ideal output would be an array of x,y Points
+//        [(x,y)(x1,y1)(x2,y2)]
+    }
 
 
 }

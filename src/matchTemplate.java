e7fb63be7a78b685a76c1e04b6075527239b4922
MichaelJahns <jahnsmic001@gmail.com>
2020-10-21 14:19:49 -0700

    Refactors for clarity, adds some more batch runner functions, organizes matchTemplate, uncomments the rectangle function and rebuilds it for working on a list of mats

--- src/matchTemplate.java
+++ src/matchTemplate.java
@@ -1,115 +1,113 @@
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class matchTemplate {
     public static int nWidth;
     public static int nHeight;
     public static String trial;
+    private static final String fileDestination = "./out/tester/";
 
+    public static Mat normalizeMat(Mat mat) {
+        Mat result = new Mat();
+        Core.normalize(mat, result, 0, 255, Core.NORM_MINMAX, CvType.CV_8UC1);
+        return result;
+    }
 
-    public static void runner(){
-        trial = "_desert";
-        String haystackPath = "images/desert.jpg";
-        Mat source = Imgcodecs.imread(haystackPath);
-        source.convertTo(source, CvType.CV_8UC1);
-        printSource(source);
+    public static void writeMat(Mat matchTemplate, String message) {
+        Imgcodecs.imwrite(fileDestination + trial + message + ".jpg", matchTemplate);
+    }
 
-        String needlePath = "images/drops.jpg";
-        Mat template = Imgcodecs.imread(needlePath);
-        template.convertTo(template, CvType.CV_8UC1);
-        printTemplate(template);
+    public static void writeListOfMats(List<Mat> matList, String message) {
+        int i = 0;
+        for (Mat mat : matList) {
+            writeMat(mat, i + message);
+            i++;
+        }
+    }
+
+    // RUNNERS
+    public static List<Mat> runner(Mat source, Mat template) {
+        trial = "algorithm_";
+
+        nHeight = template.rows();
+        nWidth = template.cols();
 
         Mat result = new Mat();
-        source.copyTo(result);
-        printPreResult(result);
 
-        printAllAlgorithms(source, template, result);
+        List<Mat> allMatchAlgorithms = matchByAllAlgorithms(source, template, result);
+        return allMatchAlgorithms;
     }
 
-    public static void printSource(Mat source){
-        System.out.println("SOURCE:");
-        System.out.println("Depth " + source.depth());
-        System.out.println("Channels " + source.channels());
-        System.out.println("Type " + source.type());
-        Imgcodecs.imwrite("out/algorithms/" + trial + "Source.jpg", source);
+    public static List<Mat> matchByAllAlgorithms(Mat source, Mat template, Mat result) {
+        List<Mat> allMatchAlgorithms = new ArrayList<>();
+
+        Mat TM_SQDIFF = matchByTM_SQDIFF(source, template, result);
+        allMatchAlgorithms.add(TM_SQDIFF);
+
+        Mat TM_SQDIFF_NORMED = matchByTM_SQDIFF_NORMED(source, template, result);
+        allMatchAlgorithms.add(TM_SQDIFF_NORMED);
+
+        Mat TM_CCORR = matchByTM_CCORR(source, template, result);
+        allMatchAlgorithms.add(TM_CCORR);
+
+        Mat TM_CCORR_NORMED = matchByTM_CCCORR_NORMED(source, template, result);
+        allMatchAlgorithms.add(TM_CCORR_NORMED);
+
+        Mat TM_CCOEFF = matchByTM_CCOEFF(source, template, result);
+        allMatchAlgorithms.add(TM_CCOEFF);
+
+        Mat TM_CCOEFF_NORMED = matchByTM_CCOEFF_NORMED(source, template, result);
+        allMatchAlgorithms.add(TM_CCOEFF_NORMED);
+
+        return allMatchAlgorithms;
     }
 
-    public static void printTemplate(Mat template){
-        System.out.println("TEMPLATE:");
-        System.out.println("Depth " + template.depth());
-        System.out.println("Channels " + template.channels());
-        System.out.println("Type " + template.type());
-        Imgcodecs.imwrite("out/algorithms/" + trial + "Template.jpg", template);
-    }
-
-    public static void printPreResult(Mat result){
-        System.out.println("RESULT:");
-        System.out.println("Depth " + result.depth());
-        System.out.println("Channels " + result.channels());
-        System.out.println("Type " + result.type());
-        Imgcodecs.imwrite("out/algorithms/" + trial + "preResult.jpg", result );
-    }
-    public static void printAllAlgorithms(Mat source, Mat template, Mat result){
-        printTM_SQDIFF(source, template,result);
-        printTM_SQDIFF_NORMED(source, template, result);
-
-        printTM_CCORR(source, template, result);
-        printTM_CCORR_NORMED(source, template, result);
-
-        printTM_CCOEFF(source, template, result);
-        printTM_CCOEFF_NORMED(source, template, result);
-    }
-    public static void printPostResult(Mat preResult, String matchAlgorithm){
-        Mat result = new Mat();
-        Core.normalize(preResult, result,0, 255, Core.NORM_MINMAX, CvType.CV_8UC1);
-        System.out.println("POST-RESULT: " + matchAlgorithm);
-        System.out.println("Depth " + result.depth());
-        System.out.println("Channels " + result.channels());
-        System.out.println("Type " + result.type());
-        Imgcodecs.imwrite("out/algorithms/" + matchAlgorithm + trial + "_result.jpg", result );
-    }
-
-    public static void printTM_SQDIFF(Mat source, Mat template, Mat result){
+    //Match Template Algorithms
+    public static Mat matchByTM_SQDIFF(Mat source, Mat template, Mat matchTemplate) {
         int machMethod = Imgproc.TM_SQDIFF;
-        String matchAlgorithm = "TM_SQDIFF";
-        Imgproc.matchTemplate(source, template, result , machMethod);
-        printPostResult(result, matchAlgorithm);
+        Imgproc.matchTemplate(source, template, matchTemplate, machMethod);
+        matchTemplate = normalizeMat(matchTemplate);
+        return matchTemplate;
     }
-    public static void printTM_SQDIFF_NORMED(Mat source, Mat template, Mat result){
+
+    public static Mat matchByTM_SQDIFF_NORMED(Mat source, Mat template, Mat matchTemplate) {
         int machMethod = Imgproc.TM_SQDIFF_NORMED;
-        String matchAlgorithm = "TM_SQDIFF_NORMED";
-        Imgproc.matchTemplate(source, template, result , machMethod);
-        printPostResult(result, matchAlgorithm);
+        Imgproc.matchTemplate(source, template, matchTemplate, machMethod);
+        matchTemplate = normalizeMat(matchTemplate);
+        return matchTemplate;
     }
-    public static void printTM_CCORR(Mat source, Mat template, Mat result){
+
+    public static Mat matchByTM_CCORR(Mat source, Mat template, Mat matchTemplate) {
         int machMethod = Imgproc.TM_CCORR;
-        String matchAlgorithm = "TM_CCORR";
-        Imgproc.matchTemplate(source, template, result , machMethod);
-        printPostResult(result, matchAlgorithm);
+        Imgproc.matchTemplate(source, template, matchTemplate, machMethod);
+        matchTemplate = normalizeMat(matchTemplate);
+        return matchTemplate;
     }
 
-    public static void printTM_CCORR_NORMED(Mat source, Mat template, Mat result){
+    public static Mat matchByTM_CCCORR_NORMED(Mat source, Mat template, Mat matchTemplate) {
         int machMethod = Imgproc.TM_CCORR_NORMED;
-        String matchAlgorithm = "TM_CCORR_NORMED";
-        Imgproc.matchTemplate(source, template, result , machMethod);
-        printPostResult(result,matchAlgorithm);
+        Imgproc.matchTemplate(source, template, matchTemplate, machMethod);
+        matchTemplate = normalizeMat(matchTemplate);
+        return matchTemplate;
     }
 
-    public static void printTM_CCOEFF(Mat source, Mat template, Mat result) {
+    public static Mat matchByTM_CCOEFF(Mat source, Mat template, Mat matchTemplate) {
         int machMethod = Imgproc.TM_CCOEFF;
-        String matchAlgorithm = "TM_CCOEFF";
-        Imgproc.matchTemplate(source, template, result , machMethod);
-        printPostResult(result, matchAlgorithm);
+        Imgproc.matchTemplate(source, template, matchTemplate, machMethod);
+        matchTemplate = normalizeMat(matchTemplate);
+        return matchTemplate;
     }
 
-    public static void printTM_CCOEFF_NORMED(Mat source, Mat template, Mat result){
+    public static Mat matchByTM_CCOEFF_NORMED(Mat source, Mat template, Mat matchTemplate) {
         int machMethod = Imgproc.TM_CCOEFF_NORMED;
-        String matchAlgorithm = "TM_CCOEFF_NORMED";
-        Imgproc.matchTemplate(source, template, result , machMethod);
-        printPostResult(result, matchAlgorithm);
+        Imgproc.matchTemplate(source, template, matchTemplate, machMethod);
+        matchTemplate = normalizeMat(matchTemplate);
+        return matchTemplate;
     }
 }
